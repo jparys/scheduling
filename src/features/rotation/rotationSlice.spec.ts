4abe2d552adcd46c23977affdac92979a9791583
Jakub Parys <jparys@gmail.com>
2021-04-18 16:23:44 +0100

    fix test

--- src/features/rotation/rotationSlice.spec.ts
+++ src/features/rotation/rotationSlice.spec.ts
@@ -5,9 +5,9 @@ import rotationReducer, {
   
   describe('rotation reducer', () => {
 
-    const initialState: RotationState = {
-        status: 'idle'
-    };
+    // const initialState: RotationState = {
+    //     status: 'idle'
+    // };
 
     it('should handle initial state', () => {
       expect(rotationReducer(undefined, { type: 'unknown' })).toEqual({
